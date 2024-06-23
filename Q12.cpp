f2f2c0183f36c1f0b23203635e5093d7e30fbfa3
LuckyBoy587 <kowshi587@gmail.com>
2024-06-23 21:24:36 +0530

    Initial

--- Q12.cpp
+++ Q12.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool hasCycleDFS(int start, int parent, map<int, vector<int>>& graph, set<int> visited) {
-    visited.insert(start);
+bool hasCycleBFS(int start, map<int, vector<int>>& graph) {
+    set<int> visited;
+    queue<pair<int, int>> q;
 
-    for (int dest: graph[start]) {
-        if (visited.find(dest) == visited.end()) {
-            if (hasCycleDFS(dest, start, graph, visited)) {
+    visited.insert(start);
+    q.push({start, -1});
+
+    while (!q.empty()) {
+        pair<int, int> currVertex = q.front();
+        q.pop();
+
+        for (int dest: graph[currVertex.first]) {
+            if (visited.find(dest) == visited.end()) {
+                visited.insert(dest);
+                q.push({dest, currVertex.first});
+            } else if (dest != currVertex.second) {
                 return true;
             }
-        } else if (dest != parent) {
-            return true;
         }
     }
 
@@ -30,7 +38,7 @@ int main(){
     }
 
     for (int start = 0; start < vertex; start++) {
-        if (hasCycleDFS(start, -1, graph, set<int>())) {
+        if (hasCycleBFS(start, graph)) {
             cout << "The social network contains cycles.";
             return 0;
         }
